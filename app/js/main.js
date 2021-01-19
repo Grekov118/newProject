21a7027b5096638c048e83a74b16d75bc667fb7a
Grekov118 <andrey.grekov.1994@mail.com>
2021-01-19 14:42:20 +0300

    added slider,footer,galery

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
-$ (function () {
+$(function () {
+
+
+  $('.slider-blog__inner').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay:true,
+    autoplaySpeed:3000,
+  });
+
+  var mixer = mixitup('.galery__content');
 
 });
